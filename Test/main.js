0cfafc60deb50d482f75611bd7a6c17d1ead28a0
Francisco <gtaskfm@gmail.com>
2018-07-28 11:01:46 +0200

    End of Lesson 8 notes

--- Test/main.js
+++ Test/main.js
@@ -1,16 +1,43 @@
 $(function () {
     var $orders = $('orders');
+    var $name = $('#name');
+    var $drink = $('#drink');
+
+    function addOrder(order) {
+        $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+    }
 
     $.ajax({
         type: 'GET',
         url: '/api/orders',
         success: function (orders) {
             $each(orders, function (i, order) {
-                $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+                addOrder(order);
             });
         },
         error: function () {
             alert('error loading orders');
         }
+    });
+
+    $('#add-order').on('click', function () {
+        //order to keep clean
+        var order = {
+            name: $name.val(),
+            drink: $drink.val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/orders',
+            data: order,
+            //if successfull ads to the page
+            success: function () {
+                addOrder(newOrder);
+            },
+            error: function () {
+                alert('error saving order')
+            }
+        })
     })
 })
