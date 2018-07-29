6095fe206b710573cd2f2e1fe0df9d5bce6ee239
Francisco <gtaskfm@gmail.com>
2018-07-29 10:47:07 +0200

    Start Lesson 5
    Getting text and HTML from the dom

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,19 +1,5 @@
-// jQuery .animate Changes from one method to another  
-
-// .animate (properties [, duration][, easing][, complete])
-// .animate (properties, options)
-// Only numeric values can be animated
-
-// Example of Animation Chain
-$("#go").click(function () {
-    $("#block")
-        .animate({
-            width: "90%"
-        }, 1000)
-        .animate({
-            fontSize: "40px"
-        }, 1000)
-        .animate({
-            borderLeftWidth: "30px"
-        }, 1000)
+// jQuery: Getting and Setting Content and Attributes   
+$("#btn").click(function () {
+    // gets the text not the html
+    console.log($("#test").text)
 });
