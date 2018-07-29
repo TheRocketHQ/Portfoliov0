7684effee8fc53c502918802a042aeddb0bd1ad5
Francisco <gtaskfm@gmail.com>
2018-07-29 09:07:01 +0200

    Update - Chaining Animations
    End of lesson 4

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,14 +1,19 @@
-// jQuery .animate
+// jQuery .animate Changes from one method to another  
 
 // .animate (properties [, duration][, easing][, complete])
 // .animate (properties, options)
+// Only numeric values can be animated
 
+// Example of Animation Chain
 $("#go").click(function () {
-    $("#block").animate({
-        opacity: .5,
-        marginLeft: "+=50",
-        height: "400px"
-    }, 3000, "linear", function () {
-        $(this).after("<div>Animation Complete</div>");
-    });
+    $("#block")
+        .animate({
+            width: "90%"
+        }, 1000)
+        .animate({
+            fontSize: "40px"
+        }, 1000)
+        .animate({
+            borderLeftWidth: "30px"
+        }, 1000)
 });
