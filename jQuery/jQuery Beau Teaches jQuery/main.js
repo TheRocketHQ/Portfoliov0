db4e7d490435a2dc1cf492c320d3141d740c273c
Francisco <gtaskfm@gmail.com>
2018-07-29 18:41:58 +0200

    Update - fixed missing comma
    Also load != than get
    Load goes directly to the element

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -3,9 +3,9 @@ function loadDoc() {
         function (responseTxt, statusTxt, xhr) {
             if (statusTxt == "success") {
                 alert("External content loaded successfully!");
-            }
+            };
             if (statusTxt == "error") {
                 alert("Error: " + xhr.status + ": " + xhr.statusTxt);
-            }
+            };
         }
 }
