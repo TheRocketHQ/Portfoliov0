8d6d0610df14a8d1b8f15ad5750a32a31b3151f5
Francisco <gtaskfm@gmail.com>
2018-07-29 16:26:41 +0200

    Update - end of lesson
    has to check if it has class

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,7 +1,11 @@
-$("button").click(function () {
+/* $("button").click(function () {
     // add class, alternative to having css in the js
     $('h1').addClass("red");
     $("p").removeClass("big");
     // adds to those who don't, remove from those who have
     $("p").toggleClass("big");
+}); */
+
+$("button").click(function () {
+    $("h2").hasClass("big");
 });
