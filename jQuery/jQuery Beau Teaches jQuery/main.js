6d8ab83011149daea20db462f30a7fafa8debecb
Francisco <gtaskfm@gmail.com>
2018-07-29 17:10:29 +0200

    Start - Lesson 9 Initial Commit

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,5 +1,10 @@
- $("span.child").nextUntil("h3").css({
-     "color": "chocolate",
-     "border": "2px solid chocolate"
- })
- //  The same happens for, prev, prevAll, prevUntil...
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt", true);
+    xhttp.send();
+}
