1ec6ad186d303bb0600879090c0c1a387db67c41
Francisco <gtaskfm@gmail.com>
2018-07-29 19:13:43 +0200

    Update - Main Readme

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,11 +1,7 @@
 function loadDoc() {
-    $("#demo").load("https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt"),
-        function (responseTxt, statusTxt, xhr) {
-            if (statusTxt == "success") {
-                alert("External content loaded successfully!");
-            };
-            if (statusTxt == "error") {
-                alert("Error: " + xhr.status + ": " + xhr.statusTxt);
-            };
-        }
+    $.get("https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt", function (data, status) {
+            console.log("Data: " + data + "/nStatus: " + status);
+        },
+        "dataType"
+    );
 }
