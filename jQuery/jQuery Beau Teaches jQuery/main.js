a138bed8419eea39cfb7025bad30de02cb1474bb
Francisco <gtaskfm@gmail.com>
2018-07-29 15:41:43 +0200

    Begin - Lesson 7 Initial Commit

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,15 +1,4 @@
-//  jQuery; Adding and Removing Elements
-//Append and prepend are going to be inside the target
-$('.target')
-    .append('<div class="child">Append</div>')
-    .prepend('<div class="child">Prepend</div>')
-    .before('<div class="child">Before</div>')
-    .after('<div class="sibling">After</div>')
-// Will append each p tag
-$('p').append('<b>Lots</b>');
+$("button").click(function () {
+    .preventDefault();
 
-// Will remove 
-$('.target').remove()
-
-// will not remove but will empty 
-$('.target').empty();
+});
