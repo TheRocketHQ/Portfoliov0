b9c41789375bced97c5f7e056b26c933e7a3e7b5
Francisco <gtaskfm@gmail.com>
2018-07-29 16:33:28 +0200

    Update - parentsUntil
    Select parent till

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,4 +1,4 @@
-$("span").parents().css({
-    "color": "chocolate",
-    "border": "2px solid chocolate"
-})
+ $("li").parentsUntil().css({
+     "color": "chocolate",
+     "border": "2px solid chocolate"
+ })
