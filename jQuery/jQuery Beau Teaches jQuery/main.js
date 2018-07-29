1c5edaeef2ba4bf4d7f67109fdcac6f993c6318e
Francisco <gtaskfm@gmail.com>
2018-07-29 19:20:22 +0200

    Update - Get
    Get get's data from a server and Post will post it to the server

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,4 +1,5 @@
 function loadDoc() {
+    // get data from a server
     $.get("https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt", function (data, status) {
             console.log("Data: " + data + "/nStatus: " + status);
         },
