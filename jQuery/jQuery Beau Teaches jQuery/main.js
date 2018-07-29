77edc49c4177bca51cbbbf87cc3de868478679d7
Francisco <gtaskfm@gmail.com>
2018-07-29 10:54:58 +0200

    Update - Setting the text

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -3,3 +3,8 @@ $("#btn").click(function () {
     // gets the attribute
     console.log($("#fcc").attr("href"))
 });
+
+$("#btn2").click(function () {
+    // Sets the attribute
+    $("#text").text("FreeCodeCamp is Awesome");
+});
