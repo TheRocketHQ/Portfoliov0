b08f1f520064c2e07ac092b8033a3e42310df245
Francisco <gtaskfm@gmail.com>
2018-07-29 18:40:38 +0200

    Update - Success / Error
    It's good practice to set up a error message, in this case specifically since if something goes wrong we need to know what was it

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,12 +1,11 @@
 function loadDoc() {
-    $("#demo").load("https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt")
-
-    //     var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function () {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         document.getElementById("demo").innerHTML = this.responseText;
-    //     }
-    // };
-    // xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt", true);
-    // xhttp.send();
+    $("#demo").load("https://cors-anywhere.herokuapp.com/http;//carnes.cc/code/ajax_example.txt"),
+        function (responseTxt, statusTxt, xhr) {
+            if (statusTxt == "success") {
+                alert("External content loaded successfully!");
+            }
+            if (statusTxt == "error") {
+                alert("Error: " + xhr.status + ": " + xhr.statusTxt);
+            }
+        }
 }
