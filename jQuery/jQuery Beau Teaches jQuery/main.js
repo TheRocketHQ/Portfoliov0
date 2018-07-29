e81534de51b96b2020432e5e88187a167a50ad28
Francisco <gtaskfm@gmail.com>
2018-07-29 16:30:56 +0200

    Start - lesson 8 start
    Initial commit

--- jQuery/jQuery Beau Teaches jQuery/main.js
+++ jQuery/jQuery Beau Teaches jQuery/main.js
@@ -1,11 +1,4 @@
-/* $("button").click(function () {
-    // add class, alternative to having css in the js
-    $('h1').addClass("red");
-    $("p").removeClass("big");
-    // adds to those who don't, remove from those who have
-    $("p").toggleClass("big");
-}); */
-
-$("button").click(function () {
-    $("h2").hasClass("big");
-});
+$("li").parent().css({
+    "color": "chocolate",
+    "border": "2px solid chocolate "
+})
