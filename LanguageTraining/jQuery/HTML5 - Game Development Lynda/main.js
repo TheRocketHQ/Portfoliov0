ca277ec7a42ca5df24b5924544acde49e42816d3
RocketRacoon <gtaskfm@gmail.com>
2018-08-03 18:53:53 +0200

    Update game layout, derivative game objects
    Resizing Canvas,
    Replacing brick for brick grid
    Getting brick x and y from the function parameters
    Ball positioning to the middle grid according to the canvas dimensions

--- LanguageTraining/jQuery/HTML5 - Game Development Lynda/main.js
+++ LanguageTraining/jQuery/HTML5 - Game Development Lynda/main.js
@@ -6,45 +6,60 @@ const BRICKS_HEIGHT = 30;
 var stage;
 var paddle;
 var ball;
+var bricks;
 
 function init() {
     stage = new createjs.Stage("demoCanvas");
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
 
-    createBrick();
+    createBrickGrid();
     createBall();
     createPaddle();
 }
 
-function createBrick() {
+function createBrickGrid() {
+    for (var i = 0; i < 14; i++)
+        for (var j = 0; j < 5; j++) {
+            createBrick(i * (BRICKS_WIDTH + 10) + 40, j * (BRICKS_HEIGHT + 5) + 20);
+        }
+}
+
+
+function createBrick(x, y) {
     var brick = new createjs.Shape();
     brick.graphics.beginFill('#000FFF');
     brick.graphics.drawRect(0, 0, BRICKS_WIDTH, BRICKS_HEIGHT);
     brick.graphics.endFill();
-    stage.addChild(brick);
+
+    brick.x = x;
+    brick.y = y;
 
     brick.regX = BRICKS_WIDTH / 2;
     brick.regY = BRICKS_HEIGHT / 2;
-
-    brick.x = 50;
-    brick.y = 50;
+    stage.addChild(brick);
 }
 
 function createBall() {
     ball = new createjs.Shape();
     ball.graphics.beginFill('red').drawCircle(0, 0, 8);
-    ball.x = 100;
-    ball.y = 100;
+    // ball.x = 100;
+    // ball.y = 100;
+    ball.x = stage.canvas.width / 2;
+    ball.y = stage.canvas.height / 2;
     stage.addChild(ball);
 
-    destroyBrick(brick);
+    destroyBrick(bricks);
 }
 
 function createPaddle() {
     paddle = new createjs.Shape();
-    paddle.graphics.beginFill('#000000').drawRect(0, 0, PADDLE_WIDTH, PADDLE_HEIGHT);
-    paddle.y = 200;
+    paddle.width = PADDLE_WIDTH;
+    paddle.height = PADDLE_HEIGHT;
+    paddle.graphics.beginFill('#333333').drawRect(0, 0, paddle.width, paddle.height);
+    paddle.x = stage.canvas.width / 2 - PADDLE_WIDTH / 2;
+    paddle.y = stage.canvas.height * 0.9;
+    stage.addChild(paddle);
     stage.addChild(paddle);
 }
 
