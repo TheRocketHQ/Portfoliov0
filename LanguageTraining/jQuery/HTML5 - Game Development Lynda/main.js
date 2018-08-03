5d2f4d9470b75c026c2fe72bee1747739cd41c79
RocketRacoon <gtaskfm@gmail.com>
2018-08-03 18:24:40 +0200

    Positiong the destroy function to animate the object in a mid direction

--- LanguageTraining/jQuery/HTML5 - Game Development Lynda/main.js
+++ LanguageTraining/jQuery/HTML5 - Game Development Lynda/main.js
@@ -1,8 +1,8 @@
 // Objects actual ones not js stuff, like for game lol it's 3am im dead
 const PADDLE_WIDTH = 100;
 const PADDLE_HEIGHT = 20;
-const BRICK_WIDTH = 60;
-const BRICK_HEIGHT = 30;
+const BRICKS_WIDTH = 60;
+const BRICKS_HEIGHT = 30;
 var stage;
 var paddle;
 var ball;
@@ -20,9 +20,15 @@ function init() {
 function createBrick() {
     var brick = new createjs.Shape();
     brick.graphics.beginFill('#000FFF');
-    brick.graphics.drawRect(0, 0, BRICK_WIDTH, BRICK_HEIGHT);
+    brick.graphics.drawRect(0, 0, BRICKS_WIDTH, BRICKS_HEIGHT);
     brick.graphics.endFill();
     stage.addChild(brick);
+
+    brick.regX = BRICKS_WIDTH / 2;
+    brick.regY = BRICKS_HEIGHT / 2;
+
+    brick.x = 50;
+    brick.y = 50;
 }
 
 function createBall() {
@@ -31,6 +37,8 @@ function createBall() {
     ball.x = 100;
     ball.y = 100;
     stage.addChild(ball);
+
+    destroyBrick(brick);
 }
 
 function createPaddle() {
@@ -38,4 +46,11 @@ function createPaddle() {
     paddle.graphics.beginFill('#000000').drawRect(0, 0, PADDLE_WIDTH, PADDLE_HEIGHT);
     paddle.y = 200;
     stage.addChild(paddle);
+}
+
+function destroyBrick(brick) {
+    createjs.Tween.get(brick, {}).to({
+        scaleX: 0,
+        scaleY: 0
+    }, 500)
 }
