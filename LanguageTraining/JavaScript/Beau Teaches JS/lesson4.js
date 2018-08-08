e9d51fd80a5bd0c80bcb002b74dc349af9aa1979
Francisco Mateus <gtaskfm@gmail.com>
2018-08-08 18:24:37 +0200

    String Basics

---
+++ LanguageTraining/JavaScript/Beau Teaches JS/lesson4.js
@@ -0,0 +1,22 @@
+/* Strings */
+var nyName = 'Beau';
+var setence = "He said \"hi!\""; // He said hi!
+var setence = 'He said "Hi!"'; // He said hi!
+
+/* 
+Code Output
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
+
+var fullName = 'Francisco ' + 'Mateus'; // 'Francisco Mateus'
+
+var setence2 = 'My  name is ' + fullname; // 'My name is Francisco Mateus
+
+fullName += ' is my name.'; // 'Francisco Mateus is my name'
