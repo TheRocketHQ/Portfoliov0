f54650ceb2678e0bfdbd423b8f30d370c43119fe
Francisco Mateus <gtaskfm@gmail.com>
2018-08-08 17:25:09 +0200

    Types of Variables

---
+++ LanguageTraining/JavaScript/Beau Teaches JS/lesson1vars.js
@@ -0,0 +1,11 @@
+var myName;
+myName = "Beau";
+var state = "Michigan";
+
+console.log("My name is " + myName);
+
+state = "California";
+
+0 - 9, a - z, A - Z
+
+var bestWebsiteEver = "freeCodeCamp";
